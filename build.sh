d90b6b47fa43905cb6936711582c5d6d7e47a5f4
Mattia225 <mattiavolpato5@gmail.com>
2024-09-18 21:27:30 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Preparing the folder..."
 mkdir -p ~/wol/Flowgorithm
 cd ~/wol/Flowgorithm/
 echo "Downloading Flowgorithm, this depends on your internet connection speed, Please wait..."
-wget https://github.com/Mattia225/g/blob/main/Flowgorithm-2.30.3-64-Setup.zip
+wget https://github.com/Mattia225/g/raw/main/Flowgorithm-2.30.3-64-Setup.zip
 mkdir ~/wol/Flowgorithm/installer
 echo "Unzipping Flowgorithm..."
 unzip ~/wol/Flowgorithm/Flowgorithm-2.30.3-64-Setup.zip -d ~/wol/Flowgorithm/installer
@@ -11,6 +11,6 @@ echo "The installer is ready to run"
 wine ~/wol/Flowgorithm/installer/"Flowgorithm Setup.msi"
 echo "The installer is finished"
 read -rsn1 -p"The installation is finished, Press any key to exit";echo
-echo clean up 
+echo clean up
 rm -rf ~/wol/Flowgorithm
 exit
