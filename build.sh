d342cf41e74161658fee0334e4bdae398cefa34f
Mattia225 <mattiavolpato5@gmail.com>
2024-10-14 19:04:00 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ echo "Preparing the folder..."
 mkdir -p ~/wol/Flowgorithm
 cd ~/wol/Flowgorithm/
 echo "Downloading Flowgorithm, this depends on your internet connection speed, Please wait..."
-wget https://github.com/Mattia225/g/raw/main/Flowgorithm-2.30.3-64-Setup.zip
+git clone https://github.com/Mattia225/g.git
 wget https://dl.winehq.org/wine/wine-mono/9.3.0/wine-mono-9.3.0-x86.msi && wine wine-mono-9.3.0-x86.msi
 mkdir ~/wol/Flowgorithm/installer
 echo "Unzipping Flowgorithm..."
-unzip ~/wol/Flowgorithm/Flowgorithm-2.30.3-64-Setup.zip -d ~/wol/Flowgorithm/installer
+unzip ~/wol/Flowgorithm/Flowgorithm-Setup.zip -d ~/wol/Flowgorithm/installer
 echo "The installer is ready to run"
 wine ~/wol/Flowgorithm/installer/"Flowgorithm Setup.msi"
 echo "The installer is finished"
